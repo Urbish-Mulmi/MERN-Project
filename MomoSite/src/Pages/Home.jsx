c1a20ebcaf39332d1f6b55a224877d445032cf8f
Urbish Mulmi <itsformaills@gmail.com>
2026-02-21 20:45:56 +0545

    Add Map component and integrate Footer into Homepage

--- MomoSite/src/Pages/Home.jsx
+++ MomoSite/src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../components/Hero'
 import { FaArrowRight } from "react-icons/fa";
 import Recipes from '../components/Recipes';
+import Map from '../components/Map';
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <Recipes/>
+        <Map/>
         
     </div>
   )
--- MomoSite/src/components/Footer.jsx
+++ MomoSite/src/components/Footer.jsx
@@ -2,8 +2,76 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        Footer 
+    <div>      
+        <div className=' flex flex-col items-center pt-[156px] pb-[55px]  px-[206px] gap-[30px]'>
+
+        {/*informational stuffs  */}
+        <div className='flex w-full justify-between'>
+
+           {/* Left section: Brand logo and company description */}
+          <div className='flex flex-col pr-4 pb-4 gap-3 w-[408px]'>
+            <div className='flex gap-2'>
+              <img src="images/logo.png" alt="logo" className='w-10 h-10' />
+              <h4 className='font-bold text-[31px] text-[#0C6967]'>momos</h4>
+            </div>
+
+            <p className="text-[#6B788E] font-normal text-[16px] w-[392px] h-[116px] ">
+              Lorem ipsum dolor sit amet consectetur. Elit sem tempor egestas molestie. Volutpat quis egestas porttitor turpis sit in. Lorem nunc nullam morbi urna amet suspendisse nullam ac vivamus.
+            </p>
+
+          </div>
+
+            {/* Right section: Footer navigation columns */}
+          <div className='flex gap-[48px]'>
+             
+             {/* Column 1: Main navigation links */}
+            <div className='flex flex-col gap-4 w-[168px] '>
+              <h4 className='font-bold text-[31px] text-[#0C6967]'>momos</h4>
+              <div className='flex flex-col gap-3'>
+                <a href="">About us</a>
+                <a href="">Our Menu</a>
+                <a href="">Our Services</a>
+              </div>
+            </div>
+            
+            {/* Column 2: Legal and support links */}
+            <div className='flex flex-col gap-4 w-[168px] '>
+              <h4 className='font-bold text-[31px] text-[#0C6967]'>Legals</h4>
+              <div className='flex flex-col gap-3'>
+                <a href="">Terms & Conditions</a>
+                <a href="">Prvacy Policy</a>
+                <a href="">Support</a>
+              </div>
+            </div>
+              
+            {/* Column 3: Social media links (icons only) */}
+            <div className='flex flex-col gap-4 w-[168px] '>
+              <h4 className='font-bold text-[31px] text-[#0C6967]'>Follow Us</h4>
+              <div className='flex  gap-4'>
+                <img src="/images/footer-icon1.png" alt="social-media-icon" className='w-10 h-10'/>
+                <img src="/images/footer-icon2.png" alt="social-media-icon" className='w-10 h-10'/>
+                <img src="/images/footer-icon3.png" alt="social-media-icon" className='w-10 h-10'/>
+                
+              </div>
+
+               <div className='flex  gap-4 w-[168px] '>
+                <img src="/images/footer-icon4.png" alt="social-media-icon" className='w-10 h-10'/>
+                <img src="/images/footer-icon5.png" alt="social-media-icon" className='w-10 h-10'/>
+                <img src="/images/footer-icon6.png" alt="social-media-icon" className='w-10 h-10'/>
+              </div>
+            </div>
+
+
+          </div>  
+
+        </div>
+
+        <img src="/images/footerline.png" alt="" />
+        
+
+        {/*Absolute-bottom copyright section */}
+        <div className='text-[#6B788E] text-[16px] font-normal'>Copyright ©️2026  Momo & Cuisines Pvt Ltd. All Rights Reserved.</div>
+        </div>
     </div>
   )
 }
