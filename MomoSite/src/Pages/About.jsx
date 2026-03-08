344c1f2b433c14494a009a3c7616eb2d6d0db877
Urbish Mulmi <itsformaills@gmail.com>
2026-03-08 12:23:16 +0545

    Add About page components

--- MomoSite/src/Pages/About.jsx
+++ MomoSite/src/Pages/About.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import AboutUsIntro from '../components/about-us/AboutUsIntro'
-
+import AboutUssecond from '../components/about-us/AboutUssecond'
+import AboutUsthird from '../components/about-us/AboutUsthird'
+import AboutUsfourth from '../components/about-us/AboutUsfourth'
+import AboutUsfifth from '../components/about-us/AboutUsfifth'
 
 const About = () => {
   return (
     <div>
         <AboutUsIntro/>
+        <AboutUssecond/>
+        <AboutUsthird/>
+        <AboutUsfourth/>
+        <AboutUsfifth/>
     </div>
   )
 }
